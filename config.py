7ab1562d1302f109ad45006cfcea64bf242e0751
Proloottricks <ritiksakhala17@gmail.com>
2024-11-20 13:12:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,8 +40,6 @@ DB_NAME = os.environ.get("DATABASE_NAME", "aquib")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1001785013016"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", ""))
-FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
